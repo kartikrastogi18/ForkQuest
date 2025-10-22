b6e0ea3424d2f66041aba7d1565c2d262690b0ff
baranwalayush <ayush6262kr@gmail.com>
2025-10-22 14:01:12 +0530

    Add linear regression

---
+++ Machine Learning/basics/linear_regression.py
@@ -0,0 +1,33 @@
+# Implementing linear regression from scratch
+
+import numpy as np
+
+class LinearRegressor:
+    def __init__(self, learning_rate, n_iterations):
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        
+        # Initialize weights and bias
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        # Gradient descent
+        for _ in range(self.n_iterations):
+            y_predicted = np.dot(X, self.weights) + self.bias
+
+            # Compute gradients
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+
+            # Update weights and bias
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+
+    def predict(self, X):
+        y_predicted = np.dot(X, self.weights) + self.bias
+        return y_predicted
---
+++ Machine Learning/basics/logistic_regression.py
@@ -0,0 +1,2 @@
+# Implementing logistic regression form scratch
+
